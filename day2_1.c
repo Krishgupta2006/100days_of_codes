41b3a13f050a0651aa2d3bd0bedb131e77be50f5
Krishgupta2006 <krrishgupta1008@gmail.com>
2025-09-24 18:07:17 +0530

    Added Day 2 code

---
+++ day2_1.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    float length, breadth, area, perimeter;
+
+    // Input length and breadth
+    printf("Enter length and breadth of the rectangle: ");
+    scanf("%f %f", &length, &breadth);
+
+    // Calculate area and perimeter
+    area = length * breadth;
+    perimeter = 2 * (length + breadth);
+
+    // Display results
+    printf("Area of rectangle = %.2f\n", area);
+    printf("Perimeter of rectangle = %.2f\n", perimeter);
+
+    return 0;
+}
